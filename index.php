75d7be1778f23530bc298df9612dbac15c542248
Mujahid <mujahid.chowdhury515@gmail.com>
2017-09-20 22:25:15 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>Half Share</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<link rel="stylesheet" href="/css/style.css" />	
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+</head>
+<body>
+<div id="image-viewer">
+<h1>Half Share Image Viewer</h1>
+<img id="main-image" class="main-image-style" src="/image/blank.png" />
+<button id="upload-image" class="upload-image-style">Upload Image</button>
+<h2>How to use:</h2>
+<p>1) Upload an image.</p><br>
+<p>2) Crop image and save.</p><br>
+<p>3) Select 'draw' or 'guess'.</p><br>
+<p>4) A URL will be generated with your image.</p>
+</div>
+<div id="timer">
+
+</div>
+
+
+// Crop function
+
+// Draw function
+
+// Guess function
+</body>
+</html>
+
+<?php // include 'includes/slider.html'; ?>
